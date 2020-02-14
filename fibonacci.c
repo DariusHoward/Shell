a885dbae6154b6c42a962d87a7524da198cbefbc
Darius J Howard <djh1997@uab.edu>
2020-02-13 22:49:14 -0600

    Upload New File

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+/*
+ *Resources used:
+ *https://www.programiz.com/c-programming/examples/fibonacci-series
+ *
+ */
+
+
+#include <stdio.h>
+int shell() {
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1; i <= n; ++i) {
+        printf("\n%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    return 0;
+}
+
